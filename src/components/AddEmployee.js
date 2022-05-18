894ff108761efae1a9b49a51fd8ddc732774aa54
anjalireghunath <anjalireghu98@gmail.com>
2022-05-18 22:26:15 +0530

    view list

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const AddEmployee = () => {
+    var [id,setId]=useState("")
+    var [name,setName]=useState("")
+    var [description,setDescription]=useState("")
+    var [salary,setsalary]=useState("")
+    var [company,setCompany]=useState("")
+    var [dob,setDob]=useState("")
+    var [email,setEmail]=useState("")
+    const alldata=()=>{
+        const data={"id":id,"name":name,"description":description,"salary":salary,"company":company,"dob":dob,"email":email}
+        console.log(data)
+    }
   return (
     <div>
 <Header/>
@@ -11,37 +22,37 @@ const AddEmployee = () => {
             <div className="row">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Employee id</label>
-                    <input type="text" className="form-control"/>
+                    <input onClick={(a)=>{setId(a.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Employee Name</label>
-                    <input type="text" className="form-control"/>
+                    <input onClick={(a)=>{setName(a.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Description</label>
-                    <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                    <textarea onChange={(a)=>{setDescription(a.target.value)}} name="" id="" cols="30" rows="10" className="form-control"></textarea>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Salary</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(a)=>{setsalary(a.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Company Name</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(a)=>{setCompany(a.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">DOB</label>
-                    <input type="date" name="" id="" class="form-control"/>
+                    <input onChange={(a)=>{setDob(a.target.value)}} type="date" name="" id="" class="form-control"/>
                      </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Email</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(a)=>{setEmail(a.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <button className="btn btn-primary">CLEAR</button>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-primary">REGISTER</button>
+                    <button onClick={alldata} className="btn btn-primary">REGISTER</button>
                 </div>
             </div>
         </div>
--- src/components/ViewEmploye.js
+++ src/components/ViewEmploye.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ViewEmploye = () => {
   return (
-    <div>ViewEmploye</div>
+    <div>
+        
+    </div>
   )
 }
 
