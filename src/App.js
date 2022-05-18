5ca0fea090703ffdf63c6ae958939330f810ace1
anjalireghunath <anjalireghu98@gmail.com>
2022-05-18 21:17:18 +0530

    add employee

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AddEmployee from './components/AddEmployee';
 function App() {
   return (
-   <div></div>
+   <div>
+     <AddEmployee/>
+   </div>
   );
 }
 
